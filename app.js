cb16d0c84ce246ef6e8ee1d31fef5be3c0963e5c
Christophe Liou Kee On <c.lioukeeon@gmail.com>
2015-11-14 22:53:56 +0100

    rotates

--- app.js
+++ app.js
@@ -31,3 +31,59 @@ app.directive('face', ['$rootScope', function ($rootScope) {
 		}
 	}
 }])
+
+app.filter('rotate', [function () {
+	return function (matrix, num) {
+		return rotate(matrix, num);
+	}
+}])
+
+app.directive('mid', ['$rootScope', function ($rootScope) {
+	return {
+		restrict: 'EA',
+		link: function(scope, element, attrs) {
+			scope.$watch(function(){
+				return scope.cube;
+			}, function(){
+				_.each(element.find('slot'), function (e) {
+					var slot = angular.element(e).scope().slot;
+					if (slot.isMiddle()) {
+						return;
+					}
+					var slotElement = angular.element(e);
+//					slot.append('aaa');
+//						console.log(lscope.slot.j == );
+//					if (lscope.slot) {
+//						
+//					}
+					var xy = slot.xy();
+					var button;
+					if (xy[0] != 0) {
+						button = angular.element('<button class="slot">&nbsp;</button>');
+						slotElement.prepend(button);
+						button.addClass(xy[0] == -1 ? 'left': 'right');
+						button.bind('click', function () {
+							
+//							console.log(slot);
+						});
+					}
+					
+					if (xy[1] != 0) {
+						button = angular.element('<button class="slot">&nbsp;</button>');
+						slotElement.prepend(button);
+						button.addClass(xy[1] == -1 ? 'top': 'bottom');
+						button.bind('click', function () {
+//							console.log(slot);
+						});
+					}
+					
+//					console.log(angular.element('<button></button>'));
+					
+				});
+//				console.log(scope.cube.faces[scope.i]);
+				
+			});
+//			console.log(attrs);
+		}
+	}
+}])
--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+
+var circular = [0,1,2,5,8,7,6,3,0];
+var sqxy = [[-1, -1], [0, -1], [1, -1], [-1, 0], [0, 0], [1, 0], [-1, 1], [0, 1], [1, 1]];
+
 function Face()
 {
 	var slots = this.slots = [];
@@ -5,42 +9,125 @@ function Face()
 	this.slots180 = [];
 	this.slots270 = [];
 	
-	this.up = function ()
+	this.relative = function (face)
 	{
-//		console.log(slots[0], slots[0].next["h0"]);
+		var i = 0, j, k, curr, next;
+		var res = [];
+//		console.log(face);
+//		return;
+		for (i = 0; i < 3; i ++) {
+			j = circular[i + (face*2)];
+//			console.log(i + (face*2), j);
+//			console.log(slots);
+			curr = face % 2 == 0 ? slots[j].vout: slots[j].hout;
+			prev = slots[j];
+			for (k = 0; k < 3; k ++) {
+//				console.log(curr);
+				res.push(curr);
+				next = curr.next[prev.id];
+				prev = curr;
+				curr = next;
+			}
+		}
+		res = rotate(res, (face+3) % 4);
+//		res = rotate(res, 0);
+//		res = rotate(res, Math.abs(face-5) % 4);
+//		var rotates = {0:1, 3:2, 2:3};
+//		if (face != 1) {
+//			res = rotate(res, rotates[face]);
+//		}
+		return res;
 	}
 }
+function Slot(id)
+{
+	var slot = this;
+	this.id = id;
+	this.next = {};
+	this.setFace = function (face) {
+		slot.face = face;
+		slot.color = face.id;
+	};
+	this.isMiddle = function () {
+		return slot.j == 4;
+	}
+	this.isCorner = function () {
+		return slot.j % 2 == 0;
+	}
+	this.isBorder = function () {
+		return slot.j % 2 == 1 && !slot.isMiddle();
+	}
+	this.xy = function () {
+		return sqxy[slot.j];
+	}
+}
+
+var rotateMatrixes = [[],[],[],[]];
+
+(function () {
+	var j, k;
+	for (j = 0; j < 3; j ++) {
+		for (k = 0; k < 3; k ++) {
+			rotateMatrixes[0][(j*3)+k] = (j*3)+k;
+			rotateMatrixes[3][(j*3)+k] = 2-j + (3*k);
+			rotateMatrixes[2][(j*3)+k] = 8 - (3*j) - k;
+			rotateMatrixes[1][(j*3)+k] = 6 + j - (3*k);
+			
+//			rotateMatrixes[0][(j*3)+k] = (j*3)+k;
+//			rotateMatrixes[1][(j*3)+k] = 2-j + (3*k);
+//			rotateMatrixes[2][(j*3)+k] = 8 - (3*j) - k;
+//			rotateMatrixes[3][(j*3)+k] = 6 + j - (3*k);
+		}
+	}
+})();
+
+function rotate(matrix, num)
+{
+	var res = [];
+	var i = 0;
+	if (num == 0) {
+		return matrix;
+	}
+	for (i = 0; i < 9; i ++) {
+		res[i] = matrix[rotateMatrixes[num][i]];
+	}
+	return res;
+}
+
 window.Cube = function()
 {
 	var table = [];
 	var faces = this.faces = [];
 	var i, j, k, l;
 	var slot, first, prev, prevprev;
+	var face;
+	
 	for (i = 0; i < 9*6; i ++) {
-		table[i] = {id: i, next: {}};
+		table[i] = new Slot(i);
 	}
 	
 	for (i = 0; i < 6; i ++) {
-		faces.push(new Face());
-		faces[i].id = i;
-//		faces.push({
-//			id: i,
-//			slots: [],
-//			slots90: [],
-//			slots180: [],
-//			slots270:[]
-//		});
+		faces.push(face = new Face());
+		face.id = i;
 		for (j = 0; j < 9; j ++) {
-			faces[i].slots.push(slot = table[(i*9)+j]);
-			slot.color = i;
+			face.slots.push(slot = table[(i*9)+j]);
+			slot.j = j;
+			slot.setFace(face);
+//			slot.color = i;
+//			slot.face = faces[i];
 		}
-		for (j = 0; j < 3; j ++) {
-			for (k = 0; k < 3; k ++) {
-				faces[i].slots90.push(table[(i*9)+ 2-j + (3*k)]);
-				faces[i].slots180.push(table[(i*9)+ 8 - (3*j) - k]);
-				faces[i].slots270.push(table[(i*9)+ 6 + j - (3*k)]);
-			}
-		}
+		face.slots90 = rotate(face.slots, 3);
+		face.slots180 = rotate(face.slots, 2);
+		face.slots270 = rotate(face.slots, 1);
+//		console.log(face.slots90, face.slots180, face.slots270);
+		
+//		for (j = 0; j < 3; j ++) {
+//			for (k = 0; k < 3; k ++) {
+//				face.slots90.push(table[(i*9)+ 2-j + (3*k)]);
+//				face.slots180.push(table[(i*9)+ 8 - (3*j) - k]);
+//				face.slots270.push(table[(i*9)+ 6 + j - (3*k)]);
+//			}
+//		}
 	}
 	
 	var flat = this.flat;
@@ -48,7 +135,7 @@ window.Cube = function()
 	var brgo_faces = [faces[2].slots, faces[3].slots, faces[4].slots, faces[5].slots];
 	var wbyg_faces = [faces[0].slots, faces[2].slots, faces[1].slots, faces[4].slots180];
 	var wryo_faces = [faces[0].slots270, faces[3].slots, faces[1].slots90, faces[5].slots180];
-
+	
 	var HORIZONTAL = 0;
 	var VERTICAL = 1;
 	flat = this.flat = constructFlat(brgo_faces, HORIZONTAL);
@@ -59,30 +146,10 @@ window.Cube = function()
 	
 	flat = this.flat = constructFlat(wryo_faces, VERTICAL);
 	linkFlatSlots(flat, VERTICAL);
-	
-	for (i = 0; i < 3; i ++) {
-		break;
-		prevprev = table[(5*9)+(i*3)+1];
-		prev = table[(5*9)+(i*3)+2];
-		for (j = 2; j < 6; j ++) {
-			for (k = 0; k < 3; k ++) {
-				slot = table[(i*3)+(j*9)+k];
-//				console.log(slot);
-				if (prevprev) {
-					prev.next[prevprev.id] = slot;
-					prev.next[slot.id] = prevprev;
-				}
-				flat.push(slot);
-				prevprev = prev;
-				prev = slot;
-			}
-		}
-		
-		prev = prevprev = null;
-	}
 
 	function linkFlatSlots(flat, mode)
 	{
+		var from, to, curr, outside, inside, vh;
 		for (i = 0; i < 3; i ++) {
 			prevprev = flat[((i+1)*12)-2];
 			prev = flat[((i+1)*12)-1];
@@ -90,14 +157,42 @@ window.Cube = function()
 				slot = flat[(i*12)+j];
 //				console.log(slot);
 				if (prevprev) {
-					prev.next[prevprev.id] = slot;
-					prev.next[slot.id] = prevprev;
-					if (mode == HORIZONTAL) {
-						prev.next["h0"] = slot;
-						prev.next["h1"] = prevprev;
-					} else if (mode == VERTICAL) {
-						prev.next["v0"] = slot;
-						prev.next["v1"] = prevprev;
+					from = prevprev;
+					to = slot;
+					curr = prev;
+					
+					curr.next[from.id] = to;
+					curr.next[to.id] = from;
+					
+					if (curr.isMiddle()) {
+					} else {
+						outside = from.face.id == curr.face.id ? to: from;
+						inside = from.face.id == curr.face.id ? from: to;
+//						if (inside.xy()[0] == sqxy[curr.j][0]) {
+						if (inside.xy()[0] == curr.xy()[0]) {
+							vh = VERTICAL;
+//						} else if (sqxy[inside.j][1] == sqxy[curr.j][1]) {
+						} else if (inside.xy()[1] == curr.xy()[1]) {
+							vh = HORIZONTAL;
+						} else {
+							throw new Error("unexpected corner");
+						}
+						if (curr.isCorner()) {
+							curr[(HORIZONTAL == vh ? 'h': 'v') + 'out'] = outside;
+						} else {
+							// border
+							if (curr.j == 1 || curr.j == 7) {
+								if (VERTICAL == vh) {
+									curr.vout = outside;
+								}
+							} else if (curr.j == 3 || curr.j == 5) {
+								if (HORIZONTAL == vh) {
+									curr.hout = outside;
+								}
+							} else {
+								throw new Error("unexpected border");
+							}
+						}
 					}
 				}
 				prevprev = prev;
@@ -115,32 +210,6 @@ window.Cube = function()
 			for (j = 0; j < 4; j ++) {
 				for (k = 0; k < 3; k ++) {
 					slot = faces[j][mode == HORIZONTAL ? (i*3)+k : (k*3)+i];
-					res.push(slot);
-				}
-			}
-		}
-		return res;
-	}
-	function constructHorizontalFlat(faces)
-	{
-		var res = [];
-		for (i = 0; i < 3; i ++) {
-			for (j = 0; j < 4; j ++) {
-				for (k = 0; k < 3; k ++) {
-					slot = faces[j][(i*3)+k];
-					res.push(slot);
-				}
-			}
-		}
-		return res;
-	}
-	function constructVerticalFlat(faces)
-	{
-		var res = [];
-		for (i = 0; i < 3; i ++) {
-			for (j = 0; j < 4; j ++) {
-				for (k = 0; k < 3; k ++) {
-					slot = faces[j][(k*3)+i];
 					res.push(slot);
 				}
 			}
